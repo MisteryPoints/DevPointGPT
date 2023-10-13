b873777f0ac17c815fd3c7959be23d384c0018f2
MisteryPoints <misterypoint2@gmailcom>
2023-10-12 23:53:09 -0400

    Adding new Chat functionality

---
+++ components/ChatRow.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+import { ChatBubbleLeftIcon, TrashIcon } from '@heroicons/react/24/outline'
+
+type Props = {
+    id: string 
+}
+
+function ChatRow({ id }: Props) {
+  return (
+    <Link href={`/chat/${id}`}>
+      <ChatBubbleLeftIcon className='h-5 w-5'/>
+      <p></p>
+      <TrashIcon className='h-5 w-5 text-gray-700 hover:text-red-700'/>
+    </Link>
+  )
+}
+
+export default ChatRow
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,22 +2,34 @@
 import Image from "next/image"
 import NewChat from "./NewChat"
 import { useSession } from 'next-auth/react'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { collection } from "firebase/firestore"
+import { db } from "@/firebase"
+import ChatRow from "./ChatRow"
 
 
 function SideBar() {
     const { data : session} = useSession();
 
+    const [chats, loading, error] = useCollection(
+      session && collection(db, 'users', session?.user?.email!, 'chats')
+    )
+    
+    console.log(chats)
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
         <div>
-            {/* NewChat */}
-            <NewChat />
-            <div>
-                {/* ModelSelection */}
-            </div>
-            {/* Map Through the ChatRows */}
-
+          {/* NewChat */}
+          <NewChat />
+          <div>
+            {/* ModelSelection */}
+          </div>
+          {/* Map Through the ChatRows */}
+          {chats?.docs.map(chat => (
+            <ChatRow key={chat.id} id={chat.id}/>
+          ))}
         </div>
       </div>
       {session && (
